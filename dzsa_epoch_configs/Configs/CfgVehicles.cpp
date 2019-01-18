93f888a53742ba08cde107d16e8c2acbc49bc17c
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2019-01-18 08:14:02 -0600

    Epoch armband
    Initial setup of dzsa_epoch_configs pbo with CfgVehicles usage for an Epoch Armband.
    
    Setup first #Includes, cfgvehicles.cpp and paths to _assets

---
+++ dzsa_epoch_configs/Configs/CfgVehicles.cpp
@@ -0,0 +1,14 @@
+class CfgVehicles
+{
+	class Armband_ColorBase;
+	class Armband_EPOCH: Armband_ColorBase
+	{
+		scope = 2;
+		author = "DirtySanchez";
+		displayName = "EPOCH Armband";
+		descriptionShort = "Epoch Rocks!";
+		visibilityModifier = 0.85;
+		color = "Epoch";
+		hiddenSelectionsTextures[] = {"\emt\dzsa_epoch_assets\DS\Armbands\armbend_epoch_g_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_small_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_small_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_big_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_big_epoch_co.paa"};
+	};
+};
