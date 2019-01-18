9f90fd6d3eec1fb5644a8b65159b57054ebfc32f
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2019-01-18 08:38:08 -0600

    test commit
    testing new webhook added retextured welding mask with cccp devils camo texture

--- dzsa_epoch_configs/Configs/CfgVehicles.cpp
+++ dzsa_epoch_configs/Configs/CfgVehicles.cpp
@@ -11,4 +11,16 @@ class CfgVehicles
 		color = "Epoch";
 		hiddenSelectionsTextures[] = {"\emt\dzsa_epoch_assets\DS\Armbands\armbend_epoch_g_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_small_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_small_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_big_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_big_epoch_co.paa"};
 	};
+	class WeldingMask;
+	class WeldingMask_DevilsCamo: WeldingMask
+	{
+		scope = 2;
+		author = "DirtySanchez";
+		displayName = "DevilsCamo Welding Mask";
+		color = "DevilsCamo";
+		visibilityModifier = 0.85;
+		noMask = 1;
+		hiddenSelectionsTextures[] = {"\emt\dzsa_epoch_assets\DS\Masks\welding_mask_devilscamo_co.paa","\emt\dzsa_epoch_assets\DS\Masks\welding_mask_devilscamo_co.paa","\emt\dzsa_epoch_assets\DS\Masks\welding_mask_devilscamo_co.paa"};
+
+	};
 };
