fbb3fdc60f282e9cc3f83f58f038b6321825fd25
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2019-01-22 08:57:18 -0600

    Update assets with Vehicle Classes for Epoch

--- dzsa_epoch_configs/Configs/CfgVehicles.cpp
+++ dzsa_epoch_configs/Configs/CfgVehicles.cpp
@@ -7,6 +7,7 @@ class CfgVehicles
 		author = "DirtySanchez";
 		displayName = "EPOCH Armband";
 		descriptionShort = "Epoch Rocks!";
+		vehicleClass = "Epoch_Clothing";
 		visibilityModifier = 0.85;
 		color = "Epoch";
 		hiddenSelectionsTextures[] = {"\emt\dzsa_epoch_assets\DS\Armbands\armbend_epoch_g_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_small_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_small_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_big_epoch_co.paa","\emt\dzsa_epoch_assets\DS\Armbands\armbend_big_epoch_co.paa"};
@@ -18,6 +19,7 @@ class CfgVehicles
 		author = "DirtySanchez";
 		displayName = "DevilsCamo Welding Mask";
 		color = "DevilsCamo";
+		vehicleClass = "Epoch_Headgear";
 		visibilityModifier = 0.85;
 		noMask = 1;
 		hiddenSelectionsTextures[] = {"\emt\dzsa_epoch_assets\DS\Masks\welding_mask_devilscamo_co.paa","\emt\dzsa_epoch_assets\DS\Masks\welding_mask_devilscamo_co.paa","\emt\dzsa_epoch_assets\DS\Masks\welding_mask_devilscamo_co.paa"};
