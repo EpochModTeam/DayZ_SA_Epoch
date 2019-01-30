96f572e65a7339e16c6a076f65f546c05aa0d9c3
vbawol <awol@veteranbastards.com>
2019-01-30 09:42:44 -0600

    test script config for 50cm_test_Wall

---
+++ dzsa_epoch_core/scripts/4_world/entities/itembase/basebuildingbase/50cm_test_Wall_Kit.c
@@ -0,0 +1,120 @@
+class 50cm_test_wall_Kit extends ItemBase
+{
+	ref protected EffectSound 						m_DeployLoopSound;
+
+	void 50cm_test_wall_Kit()
+	{
+		RegisterNetSyncVariableBool("m_IsSoundSynchRemote");
+	}
+
+	override void EEInit()
+	{
+		super.EEInit();
+
+		//set visual on init
+		UpdateVisuals();
+		UpdatePhysics();
+	}
+
+	override void OnItemLocationChanged(EntityAI old_owner, EntityAI new_owner)
+	{
+		super.OnItemLocationChanged(old_owner, new_owner);
+
+		//update visuals after location change
+		UpdatePhysics();
+	}
+
+	//Update visuals and physics
+	void UpdateVisuals()
+	{
+		SetAnimationPhase("Inventory", 0);
+		SetAnimationPhase("Placing", 1);
+	}
+
+	void UpdatePhysics()
+	{
+		AddProxyPhysics("Inventory");
+		RemoveProxyPhysics("Placing");
+	}
+
+	/*override bool IsOneHandedBehaviour()
+	{
+		return true;
+	}*/
+
+	override void OnVariablesSynchronized()
+	{
+		super.OnVariablesSynchronized();
+
+		if (IsDeploySound())
+		{
+			PlayDeploySound();
+		}
+
+		if (CanPlayDeployLoopSound())
+		{
+			PlayDeployLoopSound();
+		}
+
+		if (m_DeployLoopSound && !CanPlayDeployLoopSound())
+		{
+			StopDeployLoopSound();
+		}
+	}
+
+	//================================================================
+	// ADVANCED PLACEMENT
+	//================================================================			
+
+	override void OnPlacementComplete(Man player)
+	{
+		if (GetGame().IsServer())
+		{
+			//Create fence
+			PlayerBase player_base = PlayerBase.Cast(player);
+			vector position = player_base.GetLocalProjectionPosition();
+			vector orientation = player_base.GetLocalProjectionOrientation();
+
+			Fence fence = Fence.Cast(GetGame().CreateObject("Fence", GetPosition()));
+			fence.SetPosition(position);
+			fence.SetOrientation(orientation);
+
+			//make the kit invisible, so it can be destroyed from deploy UA when action ends
+			HideAllSelections();
+		}
+
+		SetIsDeploySound(true);
+	}
+
+	override bool IsDeployable()
+	{
+		return true;
+	}
+
+	override string GetDeploySoundset()
+	{
+		return "putDown_50cm_test_wall_Kit_SoundSet";
+	}
+
+	override string GetLoopDeploySoundset()
+	{
+		return "BarbedWire_Deploy_loop_SoundSet";
+	}
+
+	void PlayDeployLoopSound()
+	{
+		if (GetGame().IsMultiplayer() && GetGame().IsClient() || !GetGame().IsMultiplayer())
+		{
+			m_DeployLoopSound = SEffectManager.PlaySound(GetLoopDeploySoundset(), GetPosition());
+		}
+	}
+
+	void StopDeployLoopSound()
+	{
+		if (GetGame().IsMultiplayer() && GetGame().IsClient() || !GetGame().IsMultiplayer())
+		{
+			m_DeployLoopSound.SoundStop();
+			delete m_DeployLoopSound;
+		}
+	}
+}
