edc0cc3250fda2b7d5e117977ea5c9d6543f08ae
vbawol <awol@veteranbastards.com>
2019-02-11 23:19:37 -0600

    Update pluginrecipesmanagerbase.c

--- dzsa_epoch_core/scripts/4_world/classes/recipes/recipes/pluginrecipesmanagerbase.c
+++ dzsa_epoch_core/scripts/4_world/classes/recipes/recipes/pluginrecipesmanagerbase.c
@@ -2,7 +2,7 @@ modded class PluginRecipesManagerBase extends PluginBase
 {
 	override void RegisterRecipies()
 	{
-		RegisterRecipe(new Craft_test_wall_Kit);
 		super.RegisterRecipies();
+		RegisterRecipe(new Craft_test_wall_Kit);
 	}
 };
