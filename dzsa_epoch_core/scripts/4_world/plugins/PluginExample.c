7ee547f23c761435a6809e6ba8ca374caa17ed68
DESKTOP-UH65DCE\MusTanG <donkeypunch.info@gmail.com>
2019-02-27 17:53:05 -0600

    Update PluginExample.c
    Add new logging plugin and a new example method with return

--- dzsa_epoch_core/scripts/4_world/plugins/PluginExample.c
+++ dzsa_epoch_core/scripts/4_world/plugins/PluginExample.c
@@ -19,11 +19,21 @@
 */
 class PluginExample extends PluginBase
 {
+	//DEBUG
+	bool debugPluginExample = true; // This will show all widgets upon loading into server
+	
+	// Plugins
+	ref PluginProcessLogs emt_logging = PluginProcessLogs.Cast(GetPlugin(PluginProcessLogs));
+	
 	string message;
+	bool result;
 	
 	void PluginExample()
 	{
-		Print("Init: PluginExample");
+		//Print("Init: PluginExample");
+		if(debugPluginExample)
+			emt_logging.WriteLog("EpochServer","$profile:EpochServer.log","PluginExample initiated!");
+		
 		// init vars and methods here
 		message = "Hello World";
 		MyMethod(message);
@@ -32,12 +42,30 @@ class PluginExample extends PluginBase
 	void ~PluginExample()
 	{
 		// remove/delete/reset vars here
-		Print("~PluginExample Closed");
+		message = "";
+		
+		//Print("~PluginExample Closed");
+		if(debugPluginExample)
+			emt_logging.WriteLog("EpochServer","$profile:EpochServer.log","~PluginExample Closed!");
+		
 	}
 	
 	// create methods for your feature
 	void MyMethod(string str)
 	{
-		Print(str);
+		//Print(str);
+		if(IsDebugOn())
+			emt_logging.WriteLog("EpochServer","$profile:EpochServer.log","PluginExample has sent:" + str);
 	}
+	
+	// void becomes a bool because we want to return true or false
+	bool IsDebugOn()
+	{
+		result = false;
+		if(debugPluginExample)
+			result = true;
+		
+		return result;
+	}
+	
 }
